ee89da91c63d927cc037ce6f4325aa818783a9ad
Luis Bueno Archaga <luisibien@gmail.com>
2022-02-26 14:46:25 +0100

    master

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,13 @@
-let a ="5"
-let b=5
-
-
-if(a===b){
-    console.log("Son iguales")
-}else{
-    console.log("No son iguales")
-}
+fetch("https://aerodatabox.p.rapidapi.com/flights/%7BsearchBy%7D/KL1395/2020-06-10", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "aerodatabox.p.rapidapi.com",
+		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
