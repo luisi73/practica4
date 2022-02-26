9bf1bd3182decbfcb1827f427f61ceb0a4d0f852
Luis Bueno Archaga <luisibien@gmail.com>
2022-02-26 17:18:11 +0100

    master

--- docs/index.js
+++
@@ -1,9 +0,0 @@
-
-const plane_url='https://api.aviationstack.com/v1/flights?access_key=5da857a71607c95cb9ed25a9934f0f3c'
-async function getPlanesData () {
-    const response = await fetch(plane_url);
-    const data=await response.json();
-    console.log(data)
-}
-
-getPlanesData();
