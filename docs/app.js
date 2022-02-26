18f88b5418b93325e790b0003cfe44b8f4f4d39a
Luis Bueno Archaga <luisibien@gmail.com>
2022-02-26 18:33:16 +0100

    master

---
+++ docs/app.js
@@ -0,0 +1,34 @@
+const map = L.map('map').setView([0,0], 5);
+
+const pcIcon = L.icon({
+    iconUrl: 'pc-icon-29-300x300.png',
+    iconSize: [40, 32],
+    iconAnchor: [25, 16]
+  });
+
+const marcador=L.marker([0,0],{icon:pcIcon}).addTo(map);
+
+const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(map);
+
+const ipFind_url='https://freegeoip.app/json/' 
+
+async function getIp () {
+    const response = await fetch(ipFind_url);
+    const data=await response.json();
+    const {country_name,region_name,latitude, longitude}= data;
+    
+    map.setView([latitude, longitude], map.getZoom());
+    marcador.setLatLng([latitude,longitude]);
+
+    document.getElementById('pais').textContent=country_name;
+    document.getElementById('region').textContent=region_name;
+    document.getElementById('latitud').textContent=latitude.toFixed(1);
+    document.getElementById('longitud').textContent=longitude.toFixed(1);
+}
+
+getIp();
+
+setInterval(getIp, 10000)
